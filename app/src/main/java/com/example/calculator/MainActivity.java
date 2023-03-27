b121f5a6cd11475399b4440c34c3185e180b31b7
arahman6 <arahman.isrt@gmail.com>
2023-03-27 22:10:14 +0600

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mYearEt;
+    Button mCalBtn;
+    TextView mAgeTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mYearEt = findViewById(R.id.yearEditText);
+        mCalBtn = findViewById(R.id.calculator_btn);
+        mAgeTv = findViewById(R.id.ageTextView);
+
+        mCalBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                String userDOB = mYearEt.getText().toString().trim();
+                int year = Calendar.getInstance().get(Calendar.YEAR);
+
+                if(userDOB.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please enter a year.", Toast.LENGTH_SHORT).show();
+                }else if (userDOB.compareTo(String.valueOf(year)) > 0){
+                    Toast.makeText(MainActivity.this, "Year should be less than year.", Toast.LENGTH_SHORT).show();
+                } else {
+                    int myAge = year - Integer.parseInt(userDOB);
+                    mAgeTv.setText("Your age is " + myAge + " years.");
+                }
+
+            }
+        });
+
+    }
+}
